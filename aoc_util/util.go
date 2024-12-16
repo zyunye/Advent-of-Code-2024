064d259851f0643c301d9c0356f06adf3c1505df
Marrollar <n@a>
2024-12-16 01:55:30 -0500

    Pretty print util

--- aoc_util/util.go
+++ aoc_util/util.go
@@ -2,10 +2,16 @@ package aoc
 
 import (
 	"fmt"
+	"encoding/json"
 )
 
 func PrintRunes(arr [][]rune) {
 	for _, row := range arr {
 		fmt.Println(string(row))
 	}
+}
+
+func Dump(data interface{}) {
+	b, _ := json.MarshalIndent(data, "", "  ")
+	fmt.Print(string(b))
 }
